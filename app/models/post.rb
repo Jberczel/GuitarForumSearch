13bd55b686b3537387f51bf12798c3dbaec03913
Jamie <jxberc@gmail.com>
2014-07-30 08:23:04 -0400

    remove old search form query and clean file

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
 
+  # scrape guitar forum pages and create records in posts table
   def self.scrape_create
     require 'open-uri'
     url = "http://www.acousticguitarforum.com/forums/forumdisplay.php"
@@ -7,54 +8,39 @@ class Post < ActiveRecord::Base
     
     begin
       data = Nokogiri::HTML(open(url))
-
       # number of fourm pages to scrape
       count = data.css('td.vbmenu_control').text.split(' ').last.to_i 
-      rows  = data.css('#threadbits_forum_17 tr')
-      rows  = rows[7..-1]
-
-      parse_create_record(rows[0]) # creates a single record
-    
+      row = data.css('#threadbits_forum_17 tr')[7]
+      # creates a single record (skip first 7 sticky topics on forum)
+      parse_create_record(row) 
     rescue StandardError=>e
       puts "Error: #{e}"
       puts "Was not able to parse website and create records."
-
     else
-      destroy_all # delete old records
-      reset_pk_sequence # reset id to 0
-
+      destroy_all         # delete old records
+      reset_pk_sequence   # reset id to 0
+      
       1.upto(count) do |i|
         page_url = url + "&page=#{i}"
         data = Nokogiri::HTML(open(page_url))
         rows = data.css('#threadbits_forum_17 tr')
-        rows  = rows[7..-1] if i == 1 # exclude sticky topics on first page
+        rows  = rows[7..-1] if i == 1   # exclude sticky topics on first page
         parse_create_records(rows)
       end
     end
   end
 
-  # used with previous search form; replace with dataTables plugin
-  def self.search(search)
-    if search
-      where("title ILIKE ?", "%#{search}%")
-    else
-      all
-    end
-  end
-
   private
-
     def self.parse_create_record(row)
-      title       = row.css('td')[2].css('div')[0].text.strip.gsub(/\s{2,}/, ' ')
-      author      = row.css('td')[2].css('div')[1].text.strip.gsub(/\s{2,}/, ' ')
-      last_post   = row.css('td')[3].text.strip.gsub(/\s{2,}/, ' ')
-      replies     = row.css('td')[4].text
-      views       = row.css('td')[5].text
-      link        = row.at_css('td a')['href']
+      title     = row.css('td')[2].css('div')[0].text.strip.gsub(/\s{2,}/, ' ')
+      author    = row.css('td')[2].css('div')[1].text.strip.gsub(/\s{2,}/, ' ')
+      last_post = row.css('td')[3].text.strip.gsub(/\s{2,}/, ' ')
+      replies   = row.css('td')[4].text
+      views     = row.css('td')[5].text
+      link      = row.at_css('td a')['href']
 
-      Post.create!( title: title, link: link, author: author, last_post: last_post,
-                    replies: replies, views: views)
-
+      Post.create!( title: title, link: link, author: author, 
+                    last_post: last_post, replies: replies, views: views)
     end
 
     def self.parse_create_records(rows)
@@ -62,6 +48,5 @@ class Post < ActiveRecord::Base
         parse_create_record(r)
       end
     end
-
 end
 
