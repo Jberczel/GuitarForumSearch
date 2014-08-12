90f6a480d82af15b04afb0fe5df84b7ea32e6244
Jamie <jxberc@gmail.com>
2014-08-12 16:15:07 -0400

    adjust front page scrolling effect

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,20 +18,22 @@
 //= require_tree .
 
 $(document).ready(function() {
-  $('#guitar').dataTable( {
-
+  $("#guitar").dataTable( {
     "ordering": false
-  } ).fadeIn("slow");
+  });
 
-    function scrollToSection() {  
-      $('html,body').animate(
-        { scrollTop: $("#search").offset().top },'slow');
-  }
-
-  $(".btn-search").click(function() {
+  $(".btn-search").click(function() { 
      scrollToSection();
   });
 
+    //helper function for scroll effect
+   function scrollToSection() {  
+    $search = $("#search");
+    $search.fadeIn();
+    $('html,body').animate({ 
+      scrollTop: $search.offset().top },'slow');
+  }
+});
 
 
-} );
+
