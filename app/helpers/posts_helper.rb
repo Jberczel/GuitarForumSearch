41638e75e77728ad107257278ada9c2c92549125
Jamie <jxberc@gmail.com>
2014-08-28 14:41:51 -0400

    edit cache helper method

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -5,8 +5,7 @@ module PostsHelper
   end
 
   def cache_key_for_posts
-    count          = Post.count
-    max_updated_at = Post.maximum(:updated_at).try(:utc).try(:to_s, :number)
-    "posts/all-#{count}-#{max_updated_at}"
+    last_updated_at = Post.first.updated_at.try(:utc).try(:to_s, :number)
+    "/all-#{last_updated_at}"
   end
 end
