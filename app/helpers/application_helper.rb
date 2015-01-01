9fe0f18f4f049c22089583c57b2479910e2b709f
Jamie <jxberc@gmail.com>
2015-01-01 12:05:00 -0500

    add comments to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,22 @@
 module ApplicationHelper
-  # post is string with date and author of last post
+  # splits user and date for AcoustitGuitarForum pages
   def getDateAndPerson(post)
     post.split('by ')
   end
 
+  # Last_post formats
   FORMATS = [
    /(?<user>.+)\s+(?<date>\d+\/\d+\/\d+.*$)/,
    /(?<user>.+)\s+(?<date>[a-zA-Z]{3} \d+ \d+.*$)/
   ]
 
+  # Splits user and date from parsed column (last_post)
+  #
+  # ==== Examples
+  #
+  # "Professor Chaos 12/31/2014 5:57 PM"    # => ["Professor Chaos", "12,31/2014 5:57 PM"]
+  # "Johnny Cash II Dec 31 2014 11:59 PM"   # => ["Johnny Cash II", "Dec 31 2014 11:59 PM"]
+  # "-"                                     # => ["", ""]
   def format_last_post(post)
     FORMATS.each do |format|
        m = post.match(format)
